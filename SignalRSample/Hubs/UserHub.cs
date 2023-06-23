1d122fbf0184fe3a7963f5899a6788e3280ce324
Dageron <Dageron12345@mail.ru>
2023-06-23 18:22:38 +0200

    Add project files.

---
+++ SignalRSample/Hubs/UserHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSample.Hubs
+{
+    public class UserHub : Hub
+    {
+        public static int TotalViews { get; set; } = 0;
+        public static int TotalUsers { get; set; } = 0;
+
+        //методы коннект и дисконнект запускаются автоматически при подключении и отключении
+        public override Task OnConnectedAsync()
+        {
+            TotalUsers++;
+            Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            TotalUsers--;
+            Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+            return base.OnDisconnectedAsync(exception);
+        }
+        public async Task<string> NewWindowLoaded(string name) //теперь возвращаемый тип string
+        {
+            TotalViews++;
+            //send update to all clients that total views have been updated
+            //updateTotalViews расположен на client side
+            await Clients.All.SendAsync("updateTotalViews", TotalViews);
+            return $"total views from {name} - {TotalViews}";
+        }
+    }
+}
---
+++ SignalRSample/wwwroot/js/deathlyHallows.js
@@ -0,0 +1,34 @@
+﻿var cloakSpan = document.getElementById("cloakCounter");
+var stoneSpan = document.getElementById("stoneCounter");
+var wandSpan = document.getElementById("wandCounter");
+
+//create connection
+var connectionDeathlyHallows = new signalR.HubConnectionBuilder()
+    //.configureLogging(signalR.LogLevel.Information)
+    .withUrl("/hubs/deathlyhallows").build();
+
+//connect to methods that hub invokes aka receive notification from hub
+//получаем инфу от метода который вызывается в хабе
+connectionDeathlyHallows.on("updateDeathlyHallowCount", (cloak, stone, wand) => {
+    cloakSpan.innerText = cloak.toString();
+    stoneSpan.innerText = stone.toString();
+    wandSpan.innerText = wand.toString();
+});
+
+function fullfilled() {
+    //do something on start
+    //используем invoke т к ожидаем ответ от сервера в виде значений
+    //с помощью метода этого мы отображаем первичные данные в новом окне, 
+    //если не добавить то при первой загрузке ничего не будет, пока не произайдет какое - либо действие
+    connectionDeathlyHallows.invoke("GetRaceStatus").then((raceCounter) => {
+        cloakSpan.innerText = raceCounter.cloak.toString();
+        stoneSpan.innerText = raceCounter.stone.toString();
+        wandSpan.innerText = raceCounter.wand.toString();
+    })
+
+    console.log("Connection to User Hub Successful");
+}
+function rejected() {
+    //rejected logs
+}
+connectionDeathlyHallows.start().then(fullfilled, rejected);
